fcd723ea0442f7f9bf69ffeda8d9ee122c65331e
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-24 15:34:47 +0200

    Refactored to use main class.

--- APBD_07/Program.cs
+++ APBD_07/Program.cs
@@ -1,32 +1,41 @@
 using APBD_07.Repository;
 using APBD_07.Service;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace APBD_07;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddControllers().AddXmlSerializerFormatters();
-builder.Services.AddScoped<IOrderRepository, MsSqlOrderDb>();
-builder.Services.AddScoped<IProductRepository, MsSqlProductDb>();
-builder.Services.AddScoped<IWarehouseRepository, MsSqlWarehouseDb>();
-builder.Services.AddScoped<IWarehouseProductRepository, MsqlWarehouseProductRepository>();
-builder.Services.AddScoped<WarehouseFunctionsService>();
-builder.Services.AddScoped<ProcedureService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddControllers().AddXmlSerializerFormatters();
+        builder.Services.AddScoped<IOrderRepository, MsSqlOrderDb>();
+        builder.Services.AddScoped<IProductRepository, MsSqlProductDb>();
+        builder.Services.AddScoped<IWarehouseRepository, MsSqlWarehouseDb>();
+        builder.Services.AddScoped<IWarehouseProductRepository, MsqlWarehouseProductRepository>();
+        builder.Services.AddScoped<WarehouseFunctionsService>();
+        builder.Services.AddScoped<ProcedureService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+    
 }
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.Run();
